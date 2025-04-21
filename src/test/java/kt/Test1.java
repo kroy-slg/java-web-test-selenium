aea929ae1b1154e0f2b1720bf2d9c7d5e7ff6b73
Krishna <kgsing15@gmail.com>
2025-04-22 01:01:46 +0530

    first commit

--- src/test/java/kt/Test1.java
+++ src/test/java/kt/Test1.java
@@ -1,27 +1,26 @@
 package kt;
 
+import base.TestGroups;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.Test;
 
 public class Test1 {
     WebDriver driver;
 
-    @BeforeClass()
-    void setup() {
+    @BeforeClass(alwaysRun = true)
+    void setup() throws InterruptedException {
         driver = new ChromeDriver();
         driver.get("https://opensource-demo.orangehrmlive.com/");
-    }
-
-    @org.testng.annotations.Test
-    void LoginApplication() throws InterruptedException {
         Thread.sleep(10000);
-        WebElement userField = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
-        WebElement passwordField = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
-        WebElement loginButton = driver.findElement(By.xpath("//*[@id=\"app\"]/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]"));
-        userField.sendKeys("Admin");
+        WebElement userNameField = driver.findElement(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[1]/div/div[2]/input"));
+        WebElement passwordField = driver.findElement(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[2]/div/div[2]/input"));
+        WebElement loginButton = driver.findElement(By.xpath("//*[@id='app']/div[1]/div/div[1]/div/div[2]/div[2]/form/div[3]"));
+        userNameField.sendKeys("Admin");
         Thread.sleep(10000);
         passwordField.sendKeys("admin123");
         Thread.sleep(5000);
@@ -29,6 +28,17 @@ public class Test1 {
         Thread.sleep(5000);
     }
 
+    @Test(testName = "test1", priority = 1, groups = {TestGroups.SMOKE})
+    public void test1() {
+        System.out.println("test1");
+    }
+
+    @Test(testName = "test2", priority = 1, groups = {TestGroups.REGRESSION})
+    public void test2() {
+        System.out.println("test2");
+    }
+
+    @AfterClass(alwaysRun = true)
     public void tearDown(){
         driver.quit();
     }
